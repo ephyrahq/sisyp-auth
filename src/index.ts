dca032fa21be1f3e6d413961084c08bf89d717fe
Dhruv Anand <ananddhruv29@gmail.com>
2024-11-16 16:22:42 +0530

    Configured CORS on backend server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import authRouter from "./routes/auth.route";
 
@@ -7,6 +8,13 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL || "http://localhost:3000", // Next.js frontend URL
+		methods: ["GET", "POST"],
+		credentials: true, // Allow sending cookies if needed
+	})
+);
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
