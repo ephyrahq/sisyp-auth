2ad00db12336317308204aaa2c497bb0566bae34
Dhruv Anand <ananddhruv29@gmail.com>
2024-12-12 08:26:22 +0530

    Deleted unused files

--- src/routes/auth.route.ts
+++
@@ -1,9 +0,0 @@
-// Authentication Route
-import express from "express";
-import { googleAuth } from "../controllers/auth.controller";
-
-const router = express.Router();
-
-router.post("/google/callback", googleAuth as any);
-
-export default router;
--- src/routes/user.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getUserInfo } from "../controllers/user.controller";
-
-const router = express.Router();
-
-router.get("/info", getUserInfo as any);
-
-export default router;
